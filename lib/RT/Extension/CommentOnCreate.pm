ecfdb9093590e20c222a7ab6c5a7a6c530326467
Alex Vandiver <alexmv@bestpractical.com>
2014-12-15 18:00:22 -0500

    Version 1.00 releng

--- lib/RT/Extension/CommentOnCreate.pm
+++ lib/RT/Extension/CommentOnCreate.pm
@@ -1,6 +1,6 @@
 package RT::Extension::CommentOnCreate;
 
-our $VERSION = '0.06';
+our $VERSION = '1.00';
 
 use warnings;
 use strict;
@@ -11,7 +11,7 @@ use Carp;
 
 RT::Extension::CommentOnCreate - Adds an optional Comment box to Ticket Creation
 
-=head1 INSTALLATION 
+=head1 INSTALLATION
 
 =over
 
@@ -23,23 +23,13 @@ RT::Extension::CommentOnCreate - Adds an optional Comment box to Ticket Creation
 
 May need root permissions
 
-=item C<patch rt (see patches directory)>
-
-if you're running a version less than 3.8.8
-
-    patch -p1 < 3.8.7-after_message_box_callback.diff
-
-you're running a version less than 4.0.7
-
-    patch -p1 < 4.0.6-include_article_flag.diff
-
 =item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
 If you are using RT 4.2 or greater, add this line:
 
     Plugin('RT::Extension::CommentOnCreate');
 
-For earlier releases of RT 4, add this line:
+For RT 4.0, add this line:
 
     Set(@Plugins, qw(RT::Extension::CommentOnCreate));
 
@@ -55,57 +45,36 @@ or add C<RT::Extension::CommentOnCreate> to your existing C<@Plugins> line.
 
 =head1 CONFIGURATION
 
-Optional configuration options to control the width and height of the Comment textbox
+Optional configuration options to control the width and height of the
+Comment textbox:
 
     Set($CommentOnCreateWidth, 80);
     Set($CommentOnCreateHeight, 190);
 
-These only apply if you have MessageBoxRichText enabled, otherwise it will inherit the global 
-MessageBoxRichTextHeight attribute.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-rt-extension-commentoncreate@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
+These only apply if you have C<MessageBoxRichText> enabled; otherwise it
+will inherit the global C<MessageBoxRichTextHeight> attribute.
 
 =head1 AUTHOR
 
-Kevin Falcone  C<< <falcone@bestpractical.com> >>
+Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
 
+=head1 BUGS
 
-=head1 LICENCE AND COPYRIGHT
+All bugs should be reported via email to
 
-Copyright (c) 2010-2014, Best Practical Solutions, LLC.  All rights reserved.
+    L<bug-RT-Extension-CommentOnCreate@rt.cpan.org|mailto:bug-RT-Extension-CommentOnCreate@rt.cpan.org>
 
-This module is free software; you can redistribute it and/or
-modify it under the terms of version 2 of the GNU General Public License.
+or via the web at
 
+    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-CommentOnCreate>.
 
-=head1 DISCLAIMER OF WARRANTY
+=head1 LICENSE AND COPYRIGHT
 
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
+This software is Copyright (c) 2014 by Best Practical Solutions
 
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
 
 =cut
 
